e826a36d4704820ccfb2481602351c1fa566aa07
vicpoo <157589559+vicpoo@users.noreply.github.com>
2025-01-30 00:51:13 -0600

    13 errores 1/2 funcional

--- main.go
+++ main.go
@@ -4,9 +4,7 @@ import (
 	"fmt"
 
 	"github.com/gin-gonic/gin"
-	"github.com/vicpoo/APIGOproyect/core"
-	"github.com/vicpoo/APIGOproyect/src/empleados/application"
-	"github.com/vicpoo/APIGOproyect/src/empleados/domain" // Asegúrate de que este import esté presente
+	"github.com/vicpoo/APIGOproyect/src/core"
 	"github.com/vicpoo/APIGOproyect/src/empleados/infrastructure"
 )
 
@@ -14,21 +12,15 @@ func main() {
 	// Iniciar la conexión a la base de datos
 	core.InitDB()
 
-	// Crear el repositorio de empleados
-	empleadoRepo := domain.NewEmpleadoRepository(core.GetDB()) // Usar correctamente el paquete domain
-
-	// Crear el caso de uso para crear empleados
-	createEmpleado := application.NewCreateEmpleado(empleadoRepo)
-
-	// Crear el controlador
-	createEmpleadoController := infrastructure.NewCreateEmpleadoController(createEmpleado)
-
 	// Configurar el servidor HTTP
 	r := gin.Default()
-	r.POST("/empleados", createEmpleadoController.Execute)
+
+	// Inicializar el enrutador y configurar las rutas
+	router := infrastructure.NewRouter(r)
+	router.Run()
 
 	// Iniciar el servidor
-	err := r.Run(":8080")
+	err := r.Run(":8000")
 	if err != nil {
 		fmt.Println("Error al iniciar el servidor:", err)
 	}
