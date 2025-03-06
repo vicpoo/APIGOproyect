670ae6188555a9f77d94b78172c858c1d91d50c0
vicpoo <157589559+vicpoo@users.noreply.github.com>
2025-03-06 12:01:30 -0600

    cors

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/vicpoo/APIGOproyect/src/core"
 	empleadosInfra "github.com/vicpoo/APIGOproyect/src/empleados/infrastructure"
@@ -10,17 +12,28 @@ import (
 )
 
 func main() {
-
 	core.InitDB()
 
 	r := gin.Default()
 
+	// Configuración de CORS
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:4200"}, // Cambia esto según la URL de tu frontend
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+
+	// Configuración de rutas para empleados
 	empleadosRouter := empleadosInfra.NewRouter(r)
 	empleadosRouter.Run()
 
+	// Configuración de rutas para equipos
 	equiposRouter := equiposInfra.NewRouter(r)
 	equiposRouter.Run()
 
+	// Iniciar el servidor en el puerto 8000
 	err := r.Run(":8000")
 	if err != nil {
 		fmt.Println("Error al iniciar el servidor:", err)
---
+++ src/core/middleware.go
@@ -0,0 +1,23 @@
+package core
+
+import (
+	"net/http"
+)
+
+// Middleware CORS para Gorilla Mux
+func MuxCORSMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+
+		// Manejar solicitudes preflight (OPTIONS)
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
